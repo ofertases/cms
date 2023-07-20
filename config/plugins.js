901d27b4dd03a89cb4e356cde3cf2bc565d8781e
Danilo Gujansky Facco <danilo.facco@hotmail.com>
2023-07-20 20:49:52 +0100

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,12 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "strapi4-ftp-provider",
-      providerOptions: {
-        host: env("FTP_HOST"),
-        port: env("FTP_PORT"),
-        user: env("FTP_USER"),
-        password: env("FTP_PASSWORD"),
-        secure: env.bool("FTP_SECURE", false),
-        path: env("FTP_BASE_PATH"),
-        baseUrl: env("FTP_BASE_URL"),
-      },
-    },
-  },
+        provider: 'cloudinary',
+        providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+        },
+    }
+},
 });
