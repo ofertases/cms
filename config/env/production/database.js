b0aeb50fde89fa3fca776828120487f5585d3f6d
Alexandre Rodrigues Batista <40678306+xTudoS@users.noreply.github.com>
2023-08-20 13:44:54 -0300

    add ssl config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -11,7 +11,9 @@ module.exports = ({ env }) => {
         port,
         database,
         user,
-        password
+        password,
+        ssl: env.bool('DATABASE_SSL', true),
+
       },
       debug: false,
     },
