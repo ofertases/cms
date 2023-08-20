81ba48aaf919e9893584d998c384e98086c54482
Alexandre Rodrigues Batista <40678306+xTudoS@users.noreply.github.com>
2023-08-20 13:58:11 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: process.env.PORT,
+  port: env.int('PORT', process.env.PORT),
   app: {
     keys: env.array('APP_KEYS')
   },
